7dfcff568394e15d3e029615e753765bde351adb
KrishnaKumarSoni <kkssonikumarkrishna506@gmail.com>
2024-06-05 21:14:09 +0530

    Fix port binding for Heroku deployment

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
@@ -7,7 +8,9 @@ def home():
     return "Hello, World!"
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
+
 
 
 # import re
